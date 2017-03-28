6e5aa05d5d7bf15eb56bebf5aee5097d65e644e8
Matthew Garde <matthewgarde@pcapredict.com>
2017-03-28 15:34:54 +0100

    Initial Commit.

---
+++ admin/language/english/module/pcapredict.php
@@ -0,0 +1,22 @@
+<?php
+
+$_['heading_title']             = 'PCA Predict';
+
+$_['text_module']               = 'Modules';
+$_['text_save_success']         = 'Successfully updated settings.';
+$_['text_logout_success']       = 'Successfully logged out.';
+
+$_['button_account']            = 'View My Account';
+$_['button_save']               = 'Save Changes';
+$_['button_logout']             = 'Logout';
+$_['button_login']              = 'Log in';
+
+$_['link_register']             = 'register here';
+$_['link_password']             = 'Forgot your password?';
+
+$_['http_status_other']         = 'An unknown exception occured. Please try again and if the problem persists, please contact support@pcapredict.com';
+$_['login_http_status_401']     = 'The account code and/or password provided was incorrect.';
+$_['license_http_status_401']   = 'Authentication was not possible.';
+$_['license_http_status_200']   = 'Setup complete.';
+
+$_['error_permission']          = 'Warning: You do not have permission to modify module "PCA Predict"';
