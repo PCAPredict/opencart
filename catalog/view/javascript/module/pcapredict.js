3dcc3472232f44c75faa31a989f7acb749d14105
Matthew Garde <matthewgarde@pcapredict.com>
2017-04-06 10:20:12 +0100

    Due to constraints on how the control reloads and sets the country selection field, for now submitting the version to the opencart marketplace without the 3rd party checkout logic as we only need to reload in that scenario.

--- catalog/view/javascript/module/pcapredict.js
+++ catalog/view/javascript/module/pcapredict.js
@@ -1,212 +1,220 @@
+// var pca_opencart_util = (function(){
+//     return {
+//         // Checks the element with the given id for instance reference changes.
+//         // When it changes, it calls a function to run.
+//         Watcher : function Watcher(id, funcToRun) {
+            
+//             var elementId = id;
+//             var disabled = false;
+//             var oldProvinceInstance = document.getElementById(elementId);
+
+//             // does the watching.
+//             function watch() 
+//             {
+//                 var newInstance = document.getElementById(elementId);
+
+//                 if (oldProvinceInstance != newInstance) {
+
+//                     var d = new Date();
+//                     var date = " - " + d.getMinutes() + ":" + d.getSeconds() + ":" + d.getMilliseconds();
+//                     if (console && console.log) { console.log("Call Update Debounce" + date); };
+
+//                     oldProvinceInstance = newInstance;
+                    
+//                     funcToRun();
+//                 }
+
+//                 if (!disabled) {
+//                     setTimeout(watch, 200);
+//                 }
+//             };
+
+//             function stop(){
+//                 disabled = true;
+//             }
+
+//             function start(){
+//                 disabled = false;
+//                 watch();
+//             }
+
+//             return {
+//                 start : start,
+//                 stop : stop
+//             };
+//         }
+//     }
+// })();
+
 var pca_tag = (function() {
     
     var account_code = '';
     var isInited = false;
-    var currentFieldCount = null;
+    //var currentFieldCount = null;
+    //var watcher = null;
 
-    function getVisibleInputElements() {
-        return $('input[type=text],select').filter(
-            function (elem) {
-                return !!($('input[type=text],select')[elem].offsetWidth ||
-                    $('input[type=text],select')[elem].offsetHeight ||
-                    $('input[type=text],select')[elem].getClientRects().length);
-                });
-    };
+    function init(accountCode) {
+            
+        if (!isInited) {
 
-    // Poll to see if the amount of input elements has changed.
-    // If so then get the controls on the page and do a reload.
-    function pollInputFieldChange() {
-        
-        setTimeout(function () {
-            var visibleInputElements = getVisibleInputElements();
+            account_code = accountCode;
+            isInited = true;
 
-            if (currentFieldCount == null) {
-                currentFieldCount = visibleInputElements.length;
-            }
-            else if (visibleInputElements.length != currentFieldCount) {
-                currentFieldCount = visibleInputElements.length;
-                reloadCapturePlusControls();
-            }
+            (function (a, c, b, e) {
+                a[b] = a[b] || {};
+                a[b].initial = { accountCode: account_code, host: account_code + ".pcapredict.com" };
+                a[b].on = a[b].on || function () { (a[b].onq = a[b].onq || []).push(arguments) };
+                var d = c.createElement("script");
+                d.async = !0;
+                d.src = e;
+                c = c.getElementsByTagName("script")[0];
+                c.parentNode.insertBefore(d, c)
+            })(window, document, "pca", "//" + account_code + ".pcapredict.com/js/sensor.js");
 
-            pollInputFieldChange(currentFieldCount);
-        }, 1000);
-    };
-        
-    // TODO : Do not like waiting on the class, must be another way!
-    // This is for the default checkout.
-    function hasLoadedRegions(pca, provinceFieldId, provinceName) {
-        setTimeout(function () {
-            if ($('.fa-spin').length > 0) {
-                hasLoadedRegions(pca, provinceFieldId, provinceName);
-            } else {
-                if (provinceFieldId) {
-                    var latestInstance = document.getElementById(provinceFieldId);
-                    pca.setValue(latestInstance, provinceName);
-                    pca.fire(latestInstance, 'change');
+            // Fields get re-rendered when we fire a change on the field.
+            // We populate the fields here so it persists it to the session.
+            pca.on('data', function (source, key, address, variations) {
+
+                switch (source) {
+                    case "address":
+                        var provinceFieldId = null;
+                        var countryFieldId = null;
+                        for (var c = 0; c < pca.capturePlus.controls.length; c++) {
+                            var cont = pca.capturePlus.controls[c];
+                            if (cont.key == key) {
+                                for (var f = 0; f < cont.fields.length; f++) {
+
+                                    var element = pca.getElement(cont.fields[f].element);
+
+                                    if (cont.fields[f].field === '{CountryName}')
+                                    {
+                                        countryFieldId = element.id;
+                                    } 
+                                    else if (cont.fields[f].field === '{ProvinceName}') 
+                                    {
+                                        provinceFieldId = element.id;
+                                    }
+                                    else 
+                                    {
+                                        pca.fire(element, 'change');
+                                    }
+                                }
+
+                                // // Start watching for changes.
+                                // var debounceProvinceUpdate = getProvinceDebounceUpdateFunction(provinceFieldId, address.ProvinceName);
+                                // watcher = new pca_opencart_util.Watcher(provinceFieldId, debounceProvinceUpdate);
+                                // watcher.start();
+
+                                // The province list reloads when a country is selected, listen to the content changed of the selection field.
+                                $("#" + provinceFieldId).on('DOMSubtreeModified', function(event) 
+                                { 
+                                    $("#" + provinceFieldId).off('DOMSubtreeModified');
+                                    //watcher.stop();
+                                    hasLoadedRegions(pca, provinceFieldId, address.ProvinceName);
+                                });
+
+                                // Get the country field to change.
+                                var countryField = document.getElementById(countryFieldId);
+                                pca.setValue(countryField, address.CountryName);
+                                pca.fire(countryField, 'change');
+                            }
+                        }
+                        break;
                 }
+            });
 
-                // Clearing out and reloading will only have elements in the html that relate to the current active controls.
-                // TODO - this sets the country selection to what ever the settings specify, so if populated with a usa address, it can reset to uk.
-                reloadCapturePlusControls();
-            }
-        }, 500);
-    };
-
-    //var previousCountry = {};
-
-    function reloadCapturePlusControls() {
-        if (pca && pca.capturePlus && pca.capturePlus.controls && pca.capturePlus.controls.length > 0) {
-            $(".pca").empty();
-            for (var i = 0; i < pca.capturePlus.controls.length; i++) {
-                console.log("Reloaded : " + pca.capturePlus.controls[i].key);
-                //previousCountry[pca.capturePlus.controls[i].key] = pca.capturePlus.controls[i].country;
-                pca.capturePlus.controls[i].reload();
-            }
+            // For the default checkout, waits on the loaded spinner that shows when updating the country field.
+            function hasLoadedRegions(pca, provinceFieldId, provinceName) {
+                setTimeout(function () {
+                    if ($('.fa-spin').length > 0) {
+                        hasLoadedRegions(pca, provinceFieldId, provinceName);
+                    } else {
+                        if (provinceFieldId) {
+                            var latestInstance = document.getElementById(provinceFieldId);
+                            pca.setValue(latestInstance, provinceName);
+                            pca.fire(latestInstance, 'change');
+                        }
+                    }
+                }, 500);
+            };
         }
     };
+    
+    // function getVisibleInputElements() {
+    //     return $('input[type=text],select').filter(
+    //         function (elem) {
+    //             return !!($('input[type=text],select')[elem].offsetWidth ||
+    //                 $('input[type=text],select')[elem].offsetHeight ||
+    //                 $('input[type=text],select')[elem].getClientRects().length);
+    //             });
+    // };
+
+    // // Poll to see if the amount of input elements has changed.
+    // // If so then get the controls on the page and do a reload.
+    // function pollInputFieldChange() {
+        
+    //     setTimeout(function () {
+    //         var visibleInputElements = getVisibleInputElements();
+
+    //         if (currentFieldCount == null) {
+    //             currentFieldCount = visibleInputElements.length;
+    //         }
+    //         else if (visibleInputElements.length != currentFieldCount) {
+    //             currentFieldCount = visibleInputElements.length;
+    //             reloadCapturePlusControls();
+    //         }
+
+    //         pollInputFieldChange(currentFieldCount);
+    //     }, 1000);
+    // };
+
+    // function reloadCapturePlusControls() {
+    //     if (pca && pca.capturePlus && pca.capturePlus.controls && pca.capturePlus.controls.length > 0) {
+    //         $(".pca").empty();
+    //         for (var i = 0; i < pca.capturePlus.controls.length; i++) {
+    //             if (console && console.log) { console.log("Reloaded : " + pca.capturePlus.controls[i].key); };
+    //             pca.capturePlus.controls[i].reload();
+    //         }
+    //     }
+    // };
+
+    // function getProvinceDebounceUpdateFunction(elementId, provinceName) {
+
+    //     // Debounce function that will update the province field when ready.
+    //     return pca.debounce(function(elementId, provinceName) {
+
+    //         if (console && console.log) { console.log("Updating Province field"); };
+
+    //         var instance = document.getElementById(elementId);
+    //         pca.setValue(instance, provinceName);
+    //         pca.fire(instance, 'change');
+
+    //         watcher.stop();
+
+    //         reloadCapturePlusControls();
+
+    //     }, 1200);
+    // }
+
+    // function getCurrentWatcher() {
+    //     return watcher;
+    // }
 
     return {
-        init : function(accountCode) {
-            
-            if (!isInited) {
+        init : init
 
-                account_code = accountCode;
+        // getVisibleInputElements : getVisibleInputElements,
 
-                isInited = true;
-
-                (function (a, c, b, e) {
-                    a[b] = a[b] || {};
-                    a[b].initial = { accountCode: account_code, host: account_code + ".pcapredict.com" };
-                    a[b].on = a[b].on || function () { (a[b].onq = a[b].onq || []).push(arguments) };
-                    var d = c.createElement("script");
-                    d.async = !0;
-                    d.src = e;
-                    c = c.getElementsByTagName("script")[0];
-                    c.parentNode.insertBefore(d, c)
-                })(window, document, "pca", "//" + account_code + ".pcapredict.com/js/sensor.js");
-
-                // pca.on('load', function (source, key, address, variations) {
-
-                //     var keysFound = Object.keys(previousCountry).filter(function(y) { return y == key; });
-
-                //     if (keysFound.length > 0){
-                //         pca.capturePlus.controls.filter(function(x) { return x.key == key; })[0].setCountry(previousCountry[key]);
-                //     }
-                // });
-
-                // Need to update the state field when the country selector is changed, 
-                // so we also need to fire a change on the country so it can make the ajax request to get the state/regions. 
-                // This means we need a mechanism to wait for the response and then choose the region/state.
-                pca.on('data', function (source, key, address, variations) {
-
-                    switch (source) {
-                        case "address":
-                            var provinceFieldId = null;
-                            var countryFieldId = null;
-                            for (var c = 0; c < pca.capturePlus.controls.length; c++) {
-                                var cont = pca.capturePlus.controls[c];
-                                if (cont.key == key) {
-                                    for (var f = 0; f < cont.fields.length; f++) {
-
-                                        var element = pca.getElement(cont.fields[f].element);
-
-                                        if (cont.fields[f].field === '{CountryName}')
-                                        {
-                                            countryFieldId = element.id;
-                                        } 
-                                        else if (cont.fields[f].field === '{ProvinceName}') 
-                                        {
-                                            provinceFieldId = element.id;
-                                        }
-                                        else 
-                                        {
-                                            pca.fire(element, 'change');
-                                        }
-                                    }
-
-                                    // Debounce function that will update the province field when ready.
-                                    var updateProvinceField_debounce = pca.debounce(function() {
-
-                                        var d = new Date();
-                                        var date = " - " + d.getMinutes() + ":" + d.getSeconds() + ":" + d.getMilliseconds();
-                                        console.log("Updating Province field" + date);
-
-                                        var instance = document.getElementById(provinceFieldId);
-                                        pca.setValue(instance, address.ProvinceName);
-                                        pca.fire(instance, 'change');
-
-                                        disable_Watcher = true;
-
-                                        // Clearing out and reloading will only have elements in the html that relate to the current active controls.
-                                        // TODO - this sets the country selection to what ever the settings specify, so if populated with a usa address, it can reset to uk.
-                                        reloadCapturePlusControls();
-
-                                    }, 1200);
-
-                                    var oldProvinceInstance = document.getElementById(provinceFieldId);
-                                    var disable_Watcher = false;
-
-                                    // Watches for instance change of the province field.
-                                    var watcher = function () {
-                                        
-                                        var newInstance = document.getElementById(provinceFieldId);
-
-                                        if (oldProvinceInstance != newInstance) {
-
-                                            var d = new Date();
-                                            var date = " - " + d.getMinutes() + ":" + d.getSeconds() + ":" + d.getMilliseconds();
-                                            console.log("Call Update Debounce" + date);
-
-                                            oldProvinceInstance = newInstance;
-                                            count = 0;
-                                            updateProvinceField_debounce();
-                                        }
-
-                                        if (!disable_Watcher) {
-                                            setTimeout(watcher, 200);
-                                        }
-                                    };
-                                    
-                                    // Start watching for changes.
-                                    watcher();
-
-                                    // Get the country field to change.
-                                    var countryField = document.getElementById(countryFieldId);
-                                    pca.setValue(countryField, address.CountryName);
-                                    pca.fire(countryField, 'change');
-
-                                    // If the instance does not change and the content of the element changes, this will cover that scenario.
-                                    $("#" + provinceFieldId).on('DOMSubtreeModified', function(event) 
-                                    { 
-                                        $("#" + provinceFieldId).off('DOMSubtreeModified');
-
-                                        disabled_Watcher = true;
-
-                                        hasLoadedRegions(pca, provinceFieldId, address.ProvinceName);
-                                    });
-                                }
-                            }
-
-                            break;
-                    }
-                });
-            }
-        },
-
-        pollInputFieldChange : pollInputFieldChange,
-
-        getVisibleInputElements : getVisibleInputElements,
-
-        reloadCapturePlusControls : reloadCapturePlusControls
+        // getCurrentWatcher : getCurrentWatcher
     }
 })();
 
-(function() {
-    // If we load on the checkout page then setup the polling to monitor field change.
-    // This means if One-Step Checkouts are installed we can still load the control
-    // and the user should be setup without the need for futher work.
-    if (document.location.href.indexOf("checkout") > -1) 
-    { 
-        //pca_tag.pollInputFieldChange(); 
-    }
-})();
+// (function() {
+//     // If we load on the checkout page then setup the polling to monitor field change.
+//     // This means if One-Step Checkouts are installed we can still load the control
+//     // and the user should be setup without the need for futher work.
+//     if (document.location.href.indexOf("checkout") > -1) 
+//     { 
+//         pca_tag.pollInputFieldChange(); 
+//     }
+// })();
